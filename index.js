253879d894a2b61b511f1cb1b8792292b32fb1a8
Asadbecck <umirzakovasad83@gmail.com>
2022-09-09 19:01:14 +0500

    wooups

---
+++ index.js
@@ -0,0 +1,44 @@
+// document.getElementById('calc').onclick = function(){
+//     let flat = document.getElementById('flat').value;
+//     flat = parseInt('flat');
+
+
+// let flat = prompt("")
+
+//     if (flat>=1 && flat <=20) alert('The first flat');
+//     else if (flat >= 21 && flat <= 48) alert('The second flat');
+//     else if (flat >= 49 && flat <= 90) alert('The second flat');
+//     else alert('Wooups , you entered unexisting number');
+// }
+
+
+
+
+
+
+
+
+// let a = prompt("");
+
+
+// if ( a >=12 &&  a  <=17) alert('you are so young');
+// else if ( a  >= 18 &&  a  <= 26) alert('the same age as me');
+// else if ( a  >= 27 &&  a  <= 45) alert('older than me , dude you can enter');
+// else alert('Wooups , you are so old');
+
+    
+
+
+
+
+
+
+
+
+
+
+// let sum = 0 ;
+// for(let i = 0; i<=200; i++){
+//     sum = sum + i;
+// }
+// console.log(sum)
